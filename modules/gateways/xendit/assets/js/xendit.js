cc406fb9a8f1ede9dedee5b68303dde8fdf202e4
andy <andykimlong@gmail.com>
2022-04-06 03:25:49 +0000

    Fix js format code

--- modules/gateways/xendit/assets/js/xendit.js
+++ modules/gateways/xendit/assets/js/xendit.js
@@ -1,5 +1,5 @@
-Xendit.setPublishableKey( xenditParam.apiKey );
-jQuery( function( $ ) {
+Xendit.setPublishableKey(xenditParam.apiKey);
+jQuery(function ($) {
     'use strict';
 
     /**
@@ -10,29 +10,29 @@ jQuery( function( $ ) {
         /**
          * Initialize event handlers and UI state.
          */
-        init: function() {
+        init: function () {
 
             // Update CC page
-            if ( $( 'form#frmUpdateCC' ).length ) {
-                this.form = $( 'form#frmUpdateCC' );
+            if ($('form#frmUpdateCC').length) {
+                this.form = $('form#frmUpdateCC');
             }
-            if($('#btnSaveCC').length){
+            if ($('#btnSaveCC').length) {
                 this.btnSaveCC = $('#btnSaveCC');
             }
 
-            if($('.validation').length){
+            if ($('.validation').length) {
                 this.validation = $('.validation');
             }
 
-            if($('#inputCardNumber').length){
+            if ($('#inputCardNumber').length) {
                 this.inputCardNumber = $('#inputCardNumber');
                 this.inputCardNumber.payment('formatCardNumber');
             }
-            if($('#inputCardExpiry').length){
+            if ($('#inputCardExpiry').length) {
                 this.inputCardExpiry = $('#inputCardExpiry');
                 this.inputCardExpiry.payment('formatCardExpiry');
             }
-            if($('#inputCardCVV').length){
+            if ($('#inputCardCVV').length) {
                 this.inputCardCVV = $('#inputCardCVV');
                 this.inputCardCVV.payment('formatCardCVC');
             }
@@ -41,7 +41,7 @@ jQuery( function( $ ) {
             this.btnSaveCC.on('click', this.onSubmit);
             this.form.on('submit', this.onSubmit);
 
-            $( document )
+            $(document)
                 .on(
                     'change',
                     '#newCardInfo :input',
@@ -57,79 +57,95 @@ jQuery( function( $ ) {
                         '<iframe height="450" width="550" id="sample-inline-frame" name="sample-inline-frame"> </iframe>' +
                         '</div>');
 
-                    $('#three-ds-container').css({'width': '550px','height': '450px','line-height': '200px','position': 'fixed','top': '25%','left': '40%','margin-top': '-100px','margin-left': '-150px','background-color': '#ffffff','border-radius': '5px','text-align': 'center','z-index': '9999'});
+                    $('#three-ds-container').css({
+                        'width': '550px',
+                        'height': '450px',
+                        'line-height': '200px',
+                        'position': 'fixed',
+                        'top': '25%',
+                        'left': '40%',
+                        'margin-top': '-100px',
+                        'margin-left': '-150px',
+                        'background-color': '#ffffff',
+                        'border-radius': '5px',
+                        'text-align': 'center',
+                        'z-index': '9999'
+                    });
                 });
         },
 
-        isXenditChosen: function() {
+        isXenditChosen: function () {
             return $('input[name=paymentmethod]:checked').val() == "xendit" && 'new' === $('input#new:checked').val();
         },
 
-        hasToken: function() {
+        hasToken: function () {
             return 0 < $('input[name="xendit_token"]').length;
         },
 
-        hasError: function() {
+        hasError: function () {
             return 0 < $('input[name="xendit_failure_reason"]').length;
         },
 
-        block: function() {
+        block: function () {
             cc_xendit_form.btnSaveCC.prop('disabled', true);
         },
 
-        unBlock: function (){
+        unBlock: function () {
             cc_xendit_form.btnSaveCC.prop('disabled', false);
         },
 
-        handleError: function( err ) {
+        handleError: function (err) {
             var failure_reason;
-            if(typeof err != 'undefined') {
+            if (typeof err != 'undefined') {
                 failure_reason = err.message || err.error_code;
             } else {
                 failure_reason = 'We encountered an issue while processing the checkout. Please contact us. Code: 200035';
             }
             cc_xendit_form.validation.html(failure_reason);
-            cc_xendit_form.form.append( "<input type='hidden' class='xendit_cc_hidden_input' name='xendit_failure_reason' value='" + failure_reason + "'/>" );
+            cc_xendit_form.form.append("<input type='hidden' class='xendit_cc_hidden_input' name='xendit_failure_reason' value='" + failure_reason + "'/>");
             return true;
         },
 
-        toggleInputError: function(erred, input) {
+        toggleInputError: function (erred, input) {
             input.parent('.form-group').toggleClass('has-error', erred);
             return input;
         },
 
-        onSubmit: function( e ) {
+        extractMonth: function (date) {
+            var expiryArray = date.split("/");
+            return String(expiryArray[0]).length === 1 ? '0' + String(expiryArray[0]) : String(expiryArray[0]);
+        },
+
+        extractYear: function (date) {
+            var expiryArray = date.split("/");
+            var fullYear = new Date().getFullYear();
+            return String(String(fullYear).substr(0, 2) + expiryArray[1]);
+        },
+
+        onSubmit: function (e) {
             e.preventDefault();
-            if (cc_xendit_form.hasToken() || cc_xendit_form.hasError()){
+            if (cc_xendit_form.hasToken() || cc_xendit_form.hasError()) {
                 $.ajax({
                     url: $('input[name=return_url]').val(),
                     method: 'POST',
                     data: cc_xendit_form.form.serialize(),
                     dataType: 'json',
-                    success: function (response){
-                        if(!response.error){
+                    success: function (response) {
+                        if (!response.error) {
                             cc_xendit_form.form.append('<p class="text-success">Updated successful.</p>')
-                        }else{
-                            cc_xendit_form.form.append('<p class="text-danger">'+ response.message +'</p>')
+                        } else {
+                            cc_xendit_form.form.append('<p class="text-danger">' + response.message + '</p>')
                         }
 
                         cc_xendit_form.unBlock();
                     }
                 });
-            }
-            else {
+            } else {
                 cc_xendit_form.block();
 
-                var card       = cc_xendit_form.inputCardNumber.val().replace(/\s/g, '');
-                var cvn        = cc_xendit_form.inputCardCVV.val().replace(/ /g, '');
-                var expiry     = cc_xendit_form.inputCardExpiry.val().replace(/ /g, '');
-                var expiryObj    = expiry.split("/");
-                var fullYear = new Date().getFullYear();
-
-                expiryObj = {
-                    month: expiryObj[0],
-                    year: String(fullYear).substr(0, 2) + expiryObj[1]
-                };
+                var card = cc_xendit_form.inputCardNumber.val().replace(/\s/g, '');
+                var cvn = cc_xendit_form.inputCardCVV.val().replace(/ /g, '');
+                var expiry = cc_xendit_form.inputCardExpiry.val().replace(/ /g, '');
 
                 // check if all card details are not empty
                 if (!card || !cvn || !expiry) {
@@ -172,36 +188,35 @@ jQuery( function( $ ) {
                 }
 
                 var data = {
-                    "card_number"     : card,
-                    "card_exp_month"  : String(expiryObj.month).length === 1 ? '0' + String(expiryObj.month) : String(expiryObj.month),
-                    "card_exp_year"   : String(expiryObj.year),
-                    "card_cvn"        : cvn,
-                    "is_multiple_use" : true,
-                    "on_behalf_of"    : false,
-                    "currency"      : xenditParam.currency
+                    "card_number": card,
+                    "card_exp_month": cc_xendit_form.extractMonth(expiry),
+                    "card_exp_year": cc_xendit_form.extractYear(expiry),
+                    "card_cvn": cvn,
+                    "is_multiple_use": true,
+                    "on_behalf_of": false,
+                    "currency": xenditParam.currency
                 };
                 var card_type = cc_xendit_form.getCardType();
 
-                cc_xendit_form.form.append( "<input type='hidden' class='xendit_cc_hidden_input' name='xendit_card_number' value='" + data.card_number + "'/>" );
-                cc_xendit_form.form.append( "<input type='hidden' class='xendit_cc_hidden_input' name='xendit_card_exp_month' value='" + data.card_exp_month + "'/>" );
-                cc_xendit_form.form.append( "<input type='hidden' class='xendit_cc_hidden_input' name='xendit_card_exp_year' value='" + data.card_exp_year + "'/>" );
-                cc_xendit_form.form.append( "<input type='hidden' class='xendit_cc_hidden_input' name='xendit_card_cvn' value='" + data.card_cvn + "'/>" );
-                cc_xendit_form.form.append( "<input type='hidden' class='xendit_cc_hidden_input' name='xendit_card_type' value='" + card_type + "'/>" );
+                cc_xendit_form.form.append("<input type='hidden' class='xendit_cc_hidden_input' name='xendit_card_number' value='" + data.card_number + "'/>");
+                cc_xendit_form.form.append("<input type='hidden' class='xendit_cc_hidden_input' name='xendit_card_exp_month' value='" + data.card_exp_month + "'/>");
+                cc_xendit_form.form.append("<input type='hidden' class='xendit_cc_hidden_input' name='xendit_card_exp_year' value='" + data.card_exp_year + "'/>");
+                cc_xendit_form.form.append("<input type='hidden' class='xendit_cc_hidden_input' name='xendit_card_cvn' value='" + data.card_cvn + "'/>");
+                cc_xendit_form.form.append("<input type='hidden' class='xendit_cc_hidden_input' name='xendit_card_type' value='" + card_type + "'/>");
 
-                Xendit.card.createToken( data, cc_xendit_form.onTokenizationResponse );
+                Xendit.card.createToken(data, cc_xendit_form.onTokenizationResponse);
 
                 // Prevent form submitting
                 return false;
             }
         },
 
-        onCCFormChange: function(e) {
+        onCCFormChange: function (e) {
             $('.xendit_cc_hidden_input').remove();
             cc_xendit_form.unBlock();
             cc_xendit_form.validation.html("");
 
-            if (e.target.id === 'inputCardNumber')
-            {
+            if (e.target.id === 'inputCardNumber') {
                 var cardNumber = cc_xendit_form.inputCardNumber.val().replace(/\s/g, '');
                 if (Xendit.card.validateCardNumber(cardNumber)) {
                     var data = {
@@ -227,7 +242,7 @@ jQuery( function( $ ) {
             }
         },
 
-        onTokenizationResponse: function(err, response) {
+        onTokenizationResponse: function (err, response) {
             if (err) {
                 var failure_reason = err.message;
                 if (err.error_code == 'INVALID_USER_ID') {
@@ -235,7 +250,7 @@ jQuery( function( $ ) {
                 } else if (err.error_code == 'VALIDATION_ERROR') {
                     failure_reason = 'Please verify that the credit card information is correct. Code: 200003';
                 }
-                cc_xendit_form.form.append( "<input type='hidden' class='xendit_cc_hidden_input' name='xendit_failure_reason' value='" + failure_reason + "'/>" );
+                cc_xendit_form.form.append("<input type='hidden' class='xendit_cc_hidden_input' name='xendit_failure_reason' value='" + failure_reason + "'/>");
 
                 cc_xendit_form.form.submit();
                 return false;
@@ -244,10 +259,10 @@ jQuery( function( $ ) {
             cc_xendit_form.form.append("<input type='hidden' class='xendit_cc_hidden_input' name='xendit_token' value='" + token_id + "'/>");
 
             var data = {
-                "token_id"   : token_id
+                "token_id": token_id
             };
 
-            if(xenditParam.can_use_dynamic_3ds === "1"){
+            if (xenditParam.can_use_dynamic_3ds === "1") {
                 Xendit.card.threeDSRecommendation(data, cc_xendit_form.on3DSRecommendationResponse);
             } else {
                 cc_xendit_form.form.submit();
@@ -257,23 +272,23 @@ jQuery( function( $ ) {
             return false;
         },
 
-        clearToken: function() {
+        clearToken: function () {
             $('.xendit_cc_hidden_input').remove();
         },
 
-        on3DSRecommendationResponse: function(err, response) {
+        on3DSRecommendationResponse: function (err, response) {
             if (err) {
                 cc_xendit_form.form.submit();
                 return false;
             }
-            cc_xendit_form.form.append( "<input type='hidden' class='xendit_cc_hidden_input' name='xendit_should_3ds' value='" + response.should_3ds + "'/>" );
+            cc_xendit_form.form.append("<input type='hidden' class='xendit_cc_hidden_input' name='xendit_should_3ds' value='" + response.should_3ds + "'/>");
             cc_xendit_form.form.submit();
 
             return;
         },
 
         /* getting card type from WC card input class name */
-        getCardType: function() {
+        getCardType: function () {
             var class_names = $('#inputCardNumber').attr('class').split(' ');
             var index = class_names.indexOf('identified');
 
@@ -286,4 +301,4 @@ jQuery( function( $ ) {
     };
 
     cc_xendit_form.init();
-} );
+});
