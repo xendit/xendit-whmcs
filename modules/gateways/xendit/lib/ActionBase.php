3440422b6adcabd77abff9780df2517be2879d9b
andy <andykimlong@gmail.com>
2022-04-08 01:59:01 +0000

    Implement migrate class

--- modules/gateways/xendit/lib/ActionBase.php
+++ modules/gateways/xendit/lib/ActionBase.php
@@ -30,28 +30,6 @@ class ActionBase
     }
 
     /**
-     * @return void
-     */
-    public function createTable()
-    {
-        // Create database.
-        if (!Capsule::schema()->hasTable('xendit_transactions')) {
-            Capsule::schema()->create('xendit_transactions', function ($table) {
-                $table->increments('id');
-                $table->integer('invoiceid')->unsigned();
-                $table->integer('orderid')->unsigned();
-                $table->integer('relid')->unsigned();
-                $table->string('type', 100);
-                $table->string('external_id', 255);
-                $table->string('transactionid', 255);
-                $table->string('status', 100);
-                $table->string('payment_method', 255);
-                $table->timestamps();
-            });
-        }
-    }
-
-    /**
      * @return mixed
      */
     public function getXenditConfig()
